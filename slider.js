c8a7f544c1b0abadb2950d228e63344953c2eedc
Yaroslav769 <yaroslavbarsukov75@gmail.com>
2024-10-24 22:14:35 +0300

    Add files via upload

---
+++ slider.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+    $(".slider").slick({
+        arrows: true, 
+        dots: true, 
+        infinite: true,
+        slidesToShow: 3, 
+        slidesToScroll: 3, 
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1, 
+                    slidesToScroll: 1 
+                }
+            }
+        ]
+    });
+});
